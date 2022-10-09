05df7f46d4826bc5e4bf1943af144467720ba2ae
BeenJee <111928575+BeenJee@users.noreply.github.com>
2022-10-09 18:29:00 -0400

    Updated the basic view
    *Fixed edgebugging with mesh collider
    *New drink boolean activated by 'k'
    *Hitbox fix

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float MoveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
+    
+
+    [SerializeField] bool holdingDrink;
+
+    Vector2 movement;
+
+
+    //Update is unreliable for physics because varying framerate, but excellent for input. 
+    void Update()
+    {
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (movement.sqrMagnitude >= 0.01)
+        {
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+        }
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+
+        if(Input.GetKey("k")){
+            if(animator.GetBool("holdingDrink") == false){
+                animator.SetBool("holdingDrink",true);
+            } else{
+            animator.SetBool("holdingDrink",false);
+            }
+        }
+
+    }
+
+    //Fixed update is very reliable at 50 frames per second every single time. 
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * MoveSpeed * Time.fixedDeltaTime);
+        //Moves the rigid body movement times speed, and then times the time inbetween the last method calls which ensures a consistent speed of running. (no gaps inbetween running clips) 
+
+    }
+
+}
