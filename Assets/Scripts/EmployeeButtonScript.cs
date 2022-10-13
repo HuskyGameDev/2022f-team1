ae6f18f5919e054cd592fafa197652d72e93978a
Caden Swanson <cjswanso@mtu.edu>
2022-10-13 18:27:58 -0400

    Added Basic implementation of the HUD and added a script folder for organization purposes.

---
+++ Assets/Scripts/EmployeeButtonScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmployeeButtonScript : MonoBehaviour
+{
+    public void OnPress()
+    {
+        print("Employee Button Was Pressed");
+    }
+}
---
+++ Assets/Scripts/HelpButtonScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpButtonScript : MonoBehaviour
+{
+    public void OnPress()
+    {
+        print("Help Button Was Pressed");
+    }
+}
